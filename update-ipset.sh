7882dc3dee53b1a406fe9912d0cf1802edd14903
Joe Zhou <zhoushuqun@gmail.com>
2018-10-11 21:25:49 +0800

    Add argument -f to support adding extra ipset entries from files.

--- update-ipset.sh
+++ update-ipset.sh
@@ -1,25 +1,35 @@
 #!/bin/sh
 input="./expanded-delegated-apnic-latest"
+files=""
 
 usage() {
 cat << EOF
-usage: $0 [-i input] <-c cc> <-n SETNAME>
+usage: $0 <-n SETNAME> [-i input <-c cc>] [[-f file]...]
+	-n SETNAME
 	-i input expanded delegated apnic file
 	-c ISO 3166 2-letter code
-	-n SETNAME
+	-f file contains ipset entries
 EOF
 }
 
-while getopts ":i:c:n:" o; do
+while getopts ":n:i:c:f:" o; do
 	case "${o}" in
+		n)
+			setname="${OPTARG}"
+			;;
 		i)
 			input="${OPTARG}"
 			;;
 		c)
 			cc="${OPTARG}"
 			;;
-		n)
-			setname="${OPTARG}"
+		f)
+			if [ -z "${OPTARG}" -o ! -r "${OPTARG}" ]; then
+				echo "\"${OPTARG}\" does not exist."
+				usage
+				exit 1
+			fi
+			files="${files} ${OPTARG}"
 			;;
 		*)
 			usage
@@ -29,36 +39,56 @@ while getopts ":i:c:n:" o; do
 done
 shift $((OPTIND-1))
 
-if [ ! -r "${input}" -o "${#cc}" -ne 2 -o -z "${setname}" ]; then
+if [ -z "${setname}" -o \( -n "${cc}" -a ! -r "${input}" \) ]; then
 	usage
 	exit 1
 fi
 
-exist_ipset="`ipset -q list "${setname}" | sed -e '1,/Members:/d' | sort`"
-fresh_ipset="`\
-	cat "${input}" \
-	| awk -v "cc=${cc}" \
-		'BEGIN { FS = "|" } $2 == cc && $3 == "ipv4" { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' \
-	| sed 's/\/32$//g' \
-	| sort \
-	| uniq \
-`"
+update_ipset_random_setname() {
+	cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | head -c 31
+}
 
+update_ipset_ipset_list_members() {
+	ipset -q list "$1" | sed -e '1,/Members:/d'
+}
+
+# exist ipset
+exist_ipset="`update_ipset_ipset_list_members "${setname}" | sort`"
+
+# cc
+if [ -n "${cc}" ]; then
+	cc_ipset="`\
+		cat "${input}" \
+		| awk -v "cc=${cc}" \
+			'BEGIN { FS = "|" } $2 == cc && $3 == "ipv4" { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' \
+		| sed 's/\/32$//g' \
+	`"
+fi
+
+# files
+if [ -n "${files}" ]; then
+	files_lines=`cat ${files} | wc -l`
+	files_setname="`update_ipset_random_setname`"
+	ipset create "${files_setname}" hash:net maxelem ${files_lines}
+	cat ${files} | xargs -I ADD-ENTRY ipset -exist add "${files_setname}" ADD-ENTRY
+	files_ipset="`update_ipset_ipset_list_members "${files_setname}"`"
+	ipset destroy "${files_setname}"
+fi
+
+# fresh ipset
+fresh_ipset="${cc_ipset}
+${files_ipset}"
+fresh_ipset="`echo "${fresh_ipset}" | sed '/^$/d' | sort | uniq`"
+
+# refresh
 if [ "${exist_ipset}" != "${fresh_ipset}" ]; then
 	fresh_count=`echo "${fresh_ipset}" | wc -l`
-	maxelem=`
-		(ipset list -q -t "${setname}" \
-			|| echo 'Header: family inet hashsize 37268 maxelem 0') \
-		| grep maxelem \
-		| awk '{print $7}'
-	`
-
-	fresh_setname="`cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | head -c 31`"
-	ipset create "${fresh_setname}" hash:net maxelem ${fresh_count} \
-		&& echo "${fresh_ipset}" \
-			| awk -v "setname=${fresh_setname}" '/.+/ { printf("add " setname " %s\n", $0) }' \
-			| ipset -exist restore \
-		&& ipset -exist -quiet create "${setname}" hash:net \
-		; ipset swap "${setname}" "${fresh_setname}" \
-		; ipset destroy "${fresh_setname}"
+	fresh_setname="`update_ipset_random_setname`"
+	ipset create "${fresh_setname}" hash:net maxelem ${fresh_count}
+	echo "${fresh_ipset}" \
+		| awk -v "setname=${fresh_setname}" '/.+/ { printf("add " setname " %s\n", $0) }' \
+		| ipset -exist restore
+	ipset -exist -quiet create "${setname}" hash:net
+	ipset swap "${setname}" "${fresh_setname}"
+	ipset destroy "${fresh_setname}"
 fi
